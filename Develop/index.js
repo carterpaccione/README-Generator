7f173a2ace3f8909f422cac18bbbb411ba42379b
unknown <carterpaccione@gmail.com>
2024-08-09 16:33:06 -0400

    added renderLicenseBadge & renderLicenseLink funcs

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
 
+import fs from 'fs';
 import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown';
 
 // TODO: Create an array of questions for user input
 
-const questions = [""];
+const questions = [
+    "What is the title of your project?", 
+    "Enter a brief description of your application.",
+    "Enter any installation instructions.",
+    "Provide usage instructions",
+    "List your contributors.",
+    "Test instructions.",
+    "Choose an open source license",
+    "What is your GitHub username?",
+    "What is your email address?"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, (generateMarkdown))
+};
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'Apache':
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case 'GNU':
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "ISC":
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'Apache':
+      return "https://www.apache.org/licenses/LICENSE-2.0";
+    case 'GNU':
+      return "https://choosealicense.com/licenses/gpl-3.0/";
+    case "MIT":
+      return "https://choosealicense.com/licenses/mit/";
+    case "ISC":
+      return "https://choosealicense.com/licenses/isc/";
+    default:
+      return "";
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,7 +40,26 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+   ## Description
+
+  ${data.description}
+  
+  ## Installation
+  
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+  
+  ## Contributing
+
+  ${data.contributors}
+  
+  ## Tests
+  
+  ${data.tests}
+  `;
 }
 
 export default generateMarkdown;
