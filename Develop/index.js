c13b94d33303bb7e9b31df6c749ca7719fd142dc
unknown <carterpaccione@gmail.com>
2024-08-12 15:32:17 -0400

    Finished functions and created readme using app

--- Develop/index.js
+++ Develop/index.js
@@ -2,13 +2,14 @@
 
 import fs from 'fs';
 import inquirer from 'inquirer';
-import generateMarkdown from './utils/generateMarkdown';
+import generateMarkdown from './utils/generateMarkdown.js';
+import { renderLicenseBadge, renderLicenseLink, renderLicenseSection } from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 
 const questions = [
     {
-        message: "What is the title of your project?",
+        message: "Enter the title of your application.",
         type: "input",
         name: "title"
     }, 
@@ -23,45 +24,60 @@ const questions = [
         name: "installation"
     },
     {
-        message: "Provide usage instructions",
+        message: "Enter usage instructions.",
         type: "input",
         name: "usage"
     },
     {
-        message: "List your contributors.",
+        message: "Enter your contributors.",
         type: "input",
         name: "contributors"
     },
     {
-        message: "Test instructions.",
+        message: "Enter any unit tests.",
         type: "input",
         name: "test"
     },
     {
-        message: "Choose an open source license",
-        type: "select",
+        message: "Choose an open source license:",
+        type: "list",
         name: "license",
-        choices: [ "Apache", "GNU", "MIT", "ISC"]
+        choices: ["Apache", "GNU", "MIT", "ISC", "None"]
     },
     {
-        message: "What is your GitHub username?",
+        message: "Enter your GitHub username.",
         type: "input",
         name: "github"
     },
     {
-        message: "What is your email address?",
+        message: "Enter your email address.",
         type: "input",
         name: "email"
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, (generateMarkdown))
-};
+function writeToFile(fileName, data, licenseSection, licenseBadge) {
+    fs.writeFile(fileName, generateMarkdown(data, licenseSection, licenseBadge), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("README.md generated successfully!");
+        }
+    }
+)};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers.license);
+            let licenseBadge = renderLicenseBadge(answers.license);
+            let licenseLink = renderLicenseLink(answers.license);
+            let licenseSection = renderLicenseSection(licenseLink, answers.license);
+            writeToFile("README.md", answers, licenseSection, licenseBadge);
+        });
+}
 
 // Function call to initialize app
 init();
